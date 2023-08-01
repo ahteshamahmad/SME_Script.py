d79a407d965ae19b469a0b0b74c22a426f168b78
ahteshamahmad <87269013+ahteshamahmad@users.noreply.github.com>
2023-08-01 16:41:26 +0530

    Update sme_script.py

--- sme_script.py
+++ sme_script.py
@@ -9,9 +9,9 @@ user_list = os.environ.get("User_List")
 if not user_list:
     print("Error: User_List environment variable not set.")
     exit(1)
-if "ahteshamahmad" in sms:
-   print("Username " + "ahteshamahmad" + " exists in sme list")
+if username in sms:
+   print("Username " + username + " exists in sme list")
    sys.exit(0)
 else:
-   print("Username " + "ahteshamahmad" + " DOES NOT exists in sme list")
+   print("Username " + username + " DOES NOT exists in sme list")
    sys.exit(1)
