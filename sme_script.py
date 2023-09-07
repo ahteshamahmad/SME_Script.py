58b1144b9487dfa32bc6be6a8f5ac64ffdbfb510
ahteshamahmad <87269013+ahteshamahmad@users.noreply.github.com>
2023-09-08 03:26:30 +0530

    Update sme_script.py

--- sme_script.py
+++ sme_script.py
@@ -1,10 +1,8 @@
-#python test_dummy_list.py $CIRCLECI_USERNAME
-
 import os
 import sys
-
 username = sys.argv[1]
 sms = os.environ["User_List"].split(",")
+
 if username in sms:
    print("Username " + username + " exists in sme list")
    sys.exit(0)
